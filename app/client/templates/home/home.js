f3cc116d5b1071f1f015e28e4170a11c7924b5fc
Silviu D. Bojica <silviu@cubicleapps.com>
2015-03-21 19:54:01 +0400

    Display time in decimal format on task well

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -4,5 +4,14 @@ Template.Home.events({
 Template.Home.helpers({
   tasks: function () {
     return Tasks.find();
+  },
+
+  timeInHours: function () {
+    var time = this.time;
+
+    var timeInt = Math.floor(this.time / 100);
+    var timeDec = this.time % 100;
+
+    return (timeInt + '.' + timeDec);
   }
 });
--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,2 +1,17 @@
 Meteor.startup(function () {
+  // Add fixtures
+  if (Tasks.find().count() === 0) {
+    Tasks.insert({
+      description: 'A very interesting coding task',
+      time: 0
+    });
+    Tasks.insert({
+      description: 'Another interesting design task',
+      time: 125
+    });
+    Tasks.insert({
+      description: 'Yet another interesting administration task',
+      time: 50
+    });
+  }
 });
